98b26ca3072e8cf28645c974867b31fe12f5a765
Larry Claman <25161683+larryclaman@users.noreply.github.com>
2020-03-12 15:19:14 -0400

    edits

---
+++ azure-vote/build.ps1
@@ -0,0 +1,12 @@
+$regsite="azurecr.io"
+$reg="lncreg1"
+$name="voteapp"
+$file="Dockerfile-for-app-service"
+docker build -f $file -t "$reg.$regsite/$name" .
+
+<#
+docker login "$reg.$regsite" -u lncreg1 -p 
+az acr import -n lncreg1 --source  docker.io/tiangolo/uwsgi-nginx-flask:python3.6
+#>
+
+docker push "$reg.$regsite/$name"
